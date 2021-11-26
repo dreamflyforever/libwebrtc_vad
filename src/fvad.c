f60dcf4f7d3a6a70424a230cab4b23d76d99da50
jim <1150660744@qq.com>
2021-11-26 10:15:30 +0800

    add queue for record input
    Signed-off-by: jim <1150660744@qq.com>

--- src/fvad.c
+++ src/fvad.c
@@ -12,8 +12,241 @@
 #include "../include/fvad.h"
 
 #include <stdlib.h>
+#include <string.h>
 #include "vad/vad_core.h"
 
+#define __disable_irq()  
+#define __enable_irq()  
+typedef struct Circle_Queue
+{
+    uint8_t *data;    //队列数据
+    uint32_t size;    //队列大小
+    uint32_t head;    //队列头部
+    uint32_t tail;    //队列尾部
+    uint32_t len;     //队列有效数据长度
+}circle_queue_struct;
+
+/**
+  * @biref 环形队列初始化
+  * @param  circle_queue_struct队列结构体
+            data队列缓冲区
+            size队列大小
+  * @retval 0成功 1失败
+  */
+int circle_queue_init(circle_queue_struct *queue,
+                       uint8_t* data,
+                       uint32_t size)
+{
+    if(data==NULL || size<2)
+        return -1;
+    queue->data=data;
+    queue->len=0;
+    queue->size=size;
+    queue->head=0;
+    queue->tail=0;
+    return 0;
+}
+
+/**
+  * @biref 环形队列擦除
+  * @param  circle_queue_struct队列结构体
+  * @retval 0成功 1失败
+  */
+int circle_queue_erase(circle_queue_struct *queue)
+{
+    if(queue==NULL)
+        return -1;
+
+    __disable_irq();
+    //queue->data=NULL;
+    queue->len=0;
+    queue->size=0;
+    queue->head=0;
+    queue->tail=0;
+    __enable_irq();
+    return 0;
+}
+
+/**
+  * @biref 获取环形队列有效数据长度
+  * @retval 环形队列长度
+  */
+unsigned int circle_queue_len(circle_queue_struct *queue)
+{
+    return queue->len;
+}
+
+/**
+  * @biref 环形队列入队操作
+  * @param  circle_queue_struct队列结构体
+            data入队数据数组指针
+            len入队数据长度
+  * @retval -1错误指针为空 -2入队数据过长非法 -3队列当前空闲大小不足  -4队列满 0成功
+  */
+int circle_queue_in(circle_queue_struct *queue,
+                    uint8_t *data,
+                    uint32_t len)
+{
+  int ret=0;
+  
+  __disable_irq();
+  
+  if(queue==NULL || data==NULL)
+  {
+    ret=-1;
+    goto out;
+  }
+  if(queue->len >= queue->size)
+  {
+    ret=-2;
+    goto out;
+  }
+  if(len>(queue->size - queue->len))
+  {
+    ret=-3;
+    goto out;
+  }
+  
+  for(uint32_t i=0;i<len;i++)
+  {
+    if(queue->len >= queue->size)
+    {
+      ret=-4;
+      goto out;
+    }
+    queue->data[queue->tail]=*data++;
+    queue->tail = (++queue->tail) % (queue->size);
+    queue->len++;
+  }
+
+out:
+  __enable_irq();
+  return ret;
+}
+
+/**
+  * @biref 环形队列出队操作
+  * @param  circle_queue_struct队列结构体
+            data出队接收数据数组指针
+            len需要出队数
+  * @retval -1错误指针为空 其他：出队数据字节数
+  */
+int circle_queue_out(circle_queue_struct *queue,
+                     uint8_t *data,
+                     uint32_t len)
+{
+  int ret=0;
+  
+  __disable_irq();
+  
+  if(queue==NULL || data==NULL)
+  {
+    ret=-1;
+    goto out;
+  }
+  if(queue->len==0)
+  {
+    ret=0;
+    goto out;
+  }
+  if(queue->len<len)
+    len=queue->len;
+  
+  for(uint32_t i=0;i<len;i++)
+  {
+    *data = queue->data[queue->head];
+    data++;
+    ret++;
+    queue->head=(++queue->head) % queue->size;
+    queue->len--;
+  }
+  
+out:
+  __enable_irq();
+  return ret;
+}
+
+/**
+  * @biref 环形队列预览数据指令
+  * @param  circle_queue_struct队列结构体
+            data预览接收数据数组指针
+            len需要预览的数据字节数
+            offset预览起始数据偏移队列头字节数
+  * @note 注意此操作不会出队数据
+  * @retval -1错误指针为空 -2有效数据长度不够 其他：出队数据字节数
+  */
+int circle_queue_preview(circle_queue_struct *queue,
+                         uint8_t *data,
+                         uint32_t len,
+                         uint32_t offset)
+{
+  int ret=0;
+  uint32_t queue_head=0;
+  
+  __disable_irq();
+  if(queue==NULL || data==NULL)
+  {
+    ret=-1;
+    goto out;
+  }
+  if(queue->len==0)
+  {
+    ret=-2;
+    goto out;
+  }
+  if(queue->len < (offset+len))
+  {
+    ret=-3;
+    goto out;
+  }
+  
+  queue_head=(queue->head+offset) % queue->size;
+  for(uint32_t i=0;i<len;i++)
+  {
+    *data = queue->data[queue_head];
+    data++;
+    ret++;
+    queue_head++;
+    queue_head %= queue->size;
+  }
+  
+out:
+  __enable_irq();
+  return ret;
+}
+
+/**
+  * @biref 环形队列删除数据指令
+  * @param  circle_queue_struct队列结构体
+            len需要删除的数据字节数
+  * @retval -1错误指针为空 其他：实际删除数据字节数
+  */
+int circle_queue_delete_data(circle_queue_struct *queue,
+                             uint32_t len)
+{
+  int ret=0;
+  
+  __disable_irq();
+  if(queue==NULL)
+  {
+    ret=-1;
+    goto out;
+  }
+  if(queue->len<len)
+    len=queue->len;
+  queue->head=(queue->head+len)%queue->size;
+  queue->len-=len;
+  
+  ret=len;
+out:
+  __enable_irq();
+  return ret;
+}
+
+
+
+static char g_queue[1024+319];
+circle_queue_struct queue_entity;
 // valid sample rates in kHz
 static const int valid_rates[] = { 8, 16, 32, 48 };
 
@@ -42,6 +275,9 @@ Fvad *fvad_new(void)
     Fvad *inst = malloc(sizeof *inst);
     if (inst) fvad_reset(inst);
     inst->count = 0;
+    circle_queue_init(&queue_entity,
+                       g_queue,
+                       1024+319);
     return inst;
 }
 
@@ -97,24 +333,19 @@ static bool valid_length(size_t rate_idx, size_t length)
 
 int fvad_feed(Fvad *inst, char *buffer, size_t size)
 {
-	static size_t pos = 0;
-	static char buf[1024+304] = {0};
-	int rv;
-	if (pos != 0) {
-		memcpy(buf+pos, buffer, size);
-	} else {
-		memcpy(buf, buffer, size);
+	int s, y, rv = 0, i;
+	uint8_t buf[320] = {0};
+	uint16_t bb[160] = {0};
+	//queue_put(buffer, size);
+	circle_queue_in(&queue_entity, buffer, size);
+	s = queue_entity.len/ 320;
+	//printf("s: %d, len: %d\n", s, queue_entity.len);
+	for (i = 0; i < s; i++ ) {
+		//queue_get(buf, 160);
+		circle_queue_out(&queue_entity, (uint8_t *)buf, 320);
+		memcpy(bb, buf, 320);
+		rv = fvad_process(inst, bb, 160);
 	}
-	pos += size;
-
-	int y = pos%320;
-	int s = pos/320;
-	int i;
-	for (i = 0; i < s; i++) {
-		rv = fvad_process(inst, (short *)(buf + i*320), 160);
-	}
-	memcpy(buffer, buf + s*320, y);
-	pos += y;
 	return rv;
 }
 
@@ -129,11 +360,19 @@ int fvad_process(Fvad* inst, const int16_t* frame, size_t length)
 
 	if (rv > 0) {
 		rv = 1;
+		printf("speech...\n");
+		//inst->count = 0;
 	} else {
+		printf("slient...\n");
 		/*sum the count*/
 		inst->count++;
-		if (inst->count >= inst->time) inst->count = 0;
-		else rv = 1;
+		if (inst->count >= inst->time) {
+			inst->count = 0;
+			//circle_queue_erase(&queue_entity);
+			//memset(g_queue, 0, 1024+319);
+		} else {
+			rv = 1;
+		}
 	}
 
 	return rv;
